ed512ba3044cc0aaf2b4db40062bd62942337075
David Phillips <davidp6@uw.edu>
2016-12-02 17:22:06 -0800

    Refactor impact analysis to handle model variants better

--- impact_analysis.r
+++ impact_analysis.r
@@ -18,8 +18,33 @@ library(ggplot2)
 # ------------------
 
 
-# --------------------------------------------------------------
-# Files, directories and settings
+# --------------------------------------------------------------------------------------
+# Settings
+
+# cutpoints - (date vector) one or two dates (not tested for all use cases with 1 cutpoint)
+# current accepted usage: '010413', '010114'
+cutpoints = as.Date(c('010413', '010114'), '%d%m%y')
+
+# slope - (logical) whether to have an intercept shift at the cutpoints or a slope shift
+# current accepted usage: TRUE
+slope = TRUE
+
+# newEffectDate - date at which to compute the effect size. NULL defaults to cutpoint 1
+# current accepted usage: '2016-06-01'
+new_effect_date = as.Date('2016-06-01')
+
+# bma_dual - (logical) whether to run BMA on ITS models that use two or one cutpoints
+# current accepted usage: FALSE
+bma_dual = TRUE
+
+# run_name - (character) extra information to describe this run. Alters file names
+# current accepted usage: ''
+run_name = '_3peice_w_modeluncertainty'
+# --------------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------
+# Files, directories
 
 # change to code directory
 codeDir = 'C:/local/mixed-methods-analysis/pcv_impact/code/'
@@ -36,13 +61,19 @@ source(paste0(codeDir, 'graph.r'))
 root = 'J:/Project/Evaluation/GAVI/Mozambique/pcv_impact/'
 
 # graph files
-itsOutcomeFile = paste0(root, 'output/its_results_slope.pdf')
-bmaFile = paste0(root, 'output/bma_results_slope.pdf')
+itsOutcomeFile = paste0(root, 'output/its_results', run_name, '.pdf')
+bmaFile = paste0(root, 'output/bma_results', run_name, '.pdf')
 
 # list of outcome variables
 outcomes = c('ipd_cases', 'ipd_pcv10_serotype_cases', 
 				'ipd_non_pcv10_serotype_cases', 'xrcp_cases')
-# --------------------------------------------------------------
+				
+# sequence and combinatorics of cutpoints
+firstCut = cutpoints[1]
+lastCut = cutpoints[2]
+cutpointSeries = seq(from=firstCut, to=lastCut, by='month')
+cutpointCombinatorics = as.Date(combn(cutpointSeries, 2), origin='1970-01-01')
+# -------------------------------------------------------------------
 
 
 # ----------------------------------------
@@ -51,44 +82,64 @@ inputData = prepData(paste0(root, 'data'))
 # ----------------------------------------
 
 
-# ------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 # Execute analysis
 
-# basic ITS across outcomes with two slopes
-cutpoints = as.Date(c('010413', '010114'), '%d%m%y')
+# basic ITS across outcomes with two cutpoints
 itsOutcomeResults = vector('list', length(outcomes)) 
 for(o in seq(length(outcomes))) {
 	itsOutcomeResults[[o]] = its(data=inputData, outcome=outcomes[o], cutpoint=cutpoints, 
-									slope=TRUE, newEffectDate=as.Date('2016-06-01'))
+									slope=slope, newEffectDate=new_effect_date)
 }
 
-# basic ITS across cut points
-firstCut = cutpoints[1]
-lastCut = cutpoints[2]
-cutpoints = seq(from=firstCut, to=lastCut, by='month')
-itsCutpointResults = vector('list', length(cutpoints)) 
-for(o in seq(length(outcomes))) {
-	for(c in seq(length(cutpoints))) {
-		itsCutpointResults[[((length(cutpoints)*(o-1))+c)]] = its(data=inputData, 
-																	outcome=outcomes[[o]], 
-																	cutpoint=cutpoints[c], 
-																	slope=TRUE,
-																	newEffectDate=as.Date('2016-06-01'))
+# basic ITS across all possible single cutpoints 
+# (within the window defined by the first and last cutpoints)
+if (!bma_dual) { 
+	itsCutpointResults1 = vector('list', length(cutpointSeries)*length(outcomes)) 
+	i=1
+	for(o in seq(length(outcomes))) {
+		for(c in seq(length(cutpointSeries))) {		
+			# run ITS on the current cutpoint
+			itsCutpointResults1[[i]] = its(data=inputData, outcome=outcomes[[o]], 
+				cutpoint=cutpointSeries[c], slope=slope, newEffectDate=new_effect_date)
+			i=i+1
+		}
 	}
 }
 
-# basic ITS across cutpoints with two slopes
-# cutpoints = as.Date(combn(cutpoints, 2), origin='1970-01-01')
-# itsCutpointResults = vector('list', ncol(cutpoints)) 
-# for(c in seq(ncol(cutpoints))) {
-	# itsCutpointResults[[c]] = its(data=inputData, outcome='ipd_pcv10_serotype_cases', 
-									# cutpoint=cutpoints[,c], slope=TRUE, newEffectDate=as.Date('2016-06-01'))
-# }
+# basic ITS across all possible pairs of cutpoints 
+# (within the window defined by the first and last cutpoints)
+if (bma_dual) { 
+	itsCutpointResults2 = vector('list', ncol(cutpointCombinatorics)*length(outcomes))
+	i=1
+	for(o in seq(length(outcomes))) {
+		for(c in seq(ncol(cutpointCombinatorics))) {
+			itsCutpointResults2[[i]] = its(data=inputData, outcome=outcomes[[o]], 
+				cutpoint=cutpointCombinatorics[,c], slope=slope, newEffectDate=new_effect_date)
+			i=i+1
+		}
+	}
+}
 
-# BMA of ITS across cut points
+# BMA of ITS across cut points (single or dual controlled by settings)
 bmaResults = vector('list', length(outcomes))
-for(o in seq(length(outcomes))) bmaResults[[o]] = bma(itsCutpointResults[((length(cutpoints)*(o-1))+1):(length(cutpoints)*o)])
-# ------------------------------------------------------------------------------------------------------------------------
+if (!bma_dual) bmaInput = itsCutpointResults1
+if (bma_dual) bmaInput = itsCutpointResults2
+for(o in seq(length(outcomes))) { 
+	# indices of combinatorics for this outcome
+	if (!bma_dual) { 
+		i1 = (o-1)*length(cutpointSeries) + 1
+		i2 = o*length(cutpointSeries)
+	}
+	if (bma_dual) { 
+		i1 = (o-1)*ncol(cutpointCombinatorics) + 1
+		i2 = o*ncol(cutpointCombinatorics)
+	}
+	
+	# average models for the current outcome
+	bmaResults[[o]] = bma(bmaInput[i1:i2])
+}
+# -----------------------------------------------------------------------------------------
 
 
 # --------------------------------------------------------------------------------------------------------------
