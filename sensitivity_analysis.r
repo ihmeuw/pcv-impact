a103db737931fca17b9fe32b73b195106b2c1a54
David Phillips <davidp6@uw.edu>
2016-12-05 10:40:00 -0800

    Formalize sensitivity analysis

--- sensitivity_analysis.r
+++ sensitivity_analysis.r
@@ -20,6 +20,9 @@ library(ggplot2)
 # -----------------------------------------------------------------------------------------
 # Files, directories, settings and lists
 
+# load impact analysis function
+source('impact_analysis.r')
+
 # settings
 run_name = 'window_variants_3peice'
 
@@ -35,23 +38,30 @@ root = paste0(j, '/Project/Evaluation/GAVI/Mozambique/pcv_impact/')
 # graph files
 graphFile = paste0(root, 'visualizations/sensitivity_', run_name, '.pdf')
 
-# run names for output files (from impact_analysis) to load in here
-input_names = c('3peice_narrow', '3peice_mednarrow', '3peice_medium', 
-				'3peice_medwide', '3peice_wide', '3peice_max3')
-
 # output data files (from impact_analysis.r)
 outputFileStub = paste0(root, 'data/output/bma_results_')
 # -----------------------------------------------------------------------------------------
 
 
-# -----------------------------------------------------------------------------------------
-# Load/prep data
+# -------------------------------------------------------------------------------------
+# Run impact_analysis.r on varying windows
 
-# load all files
-modelOutput = lapply(paste0(outputFileStub, input_names, '.rdata'), function(x) { 
-	load(x)
+# store window end dates
+endDates = seq.Date(from=as.Date('2013-05-01'), to=as.Date('2016-04-01'), by='month')
+
+# run for each date
+modelOutput = lapply(endDates, function(endDate) { 
+	cutpoints = c(as.Date('2013-01-01'), endDate)
+	bmaResults = impactAnalysis(cutpoints=cutpoints, 
+									run_name=as.character(endDate), 
+									bma_dual=TRUE)
 	return(bmaResults)
 })
+# -------------------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------------------
+# Load/prep data
 
 # assemble effect estimates into a workable data table
 assembleData = function(objName) { 
@@ -61,7 +71,7 @@ assembleData = function(objName) {
 			tmp = modelOutput[[run]][[outcome]][[objName]]
 			tmpOut = modelOutput[[run]][[outcome]]$outcome
 			tmp = data.table(tmp)
-			tmp[, run:=input_names[run]]
+			tmp[, run:=endDates[run]]
 			tmp[, outcome:=tmpOut]
 			output = rbind(output, tmp, fill=TRUE)
 		}
@@ -73,7 +83,7 @@ effectSizes = assembleData('effect_size')
 # prep effect sizes/labels
 effectSizes[, est:=c('Estimate', 'Upper', 'Lower', 'se')]
 effectSizes = effectSizes[est!='se']
-effectSizes[, run_id:=match(run, input_names)]
+effectSizes[, run_id:=match(run, endDates)]
 effectSizes[, effect:=(exp(effect)*100)-100]
 effectSizes[outcome=='ipd_cases', outcome_label:='All IPD Cases']
 effectSizes[outcome=='ipd_pcv10_serotype_cases', outcome_label:='PCV10 Serotypes']
@@ -84,7 +94,7 @@ effectSizes[outcome=='xrcp_cases', outcome_label:='All X−Ray Confirmed Cases']
 fittedValues = assembleData('data')
 
 # prep fitted values/labels
-fittedValues[, run_id:=match(run, input_names)]
+fittedValues[, run_id:=match(run, endDates)]
 fittedValues[outcome=='ipd_cases', outcome_label:='All IPD Cases']
 fittedValues[outcome=='ipd_pcv10_serotype_cases', outcome_label:='PCV10 Serotypes']
 fittedValues[outcome=='ipd_non_pcv10_serotype_cases', outcome_label:='Non−PCV10 Serotypes']
@@ -97,6 +107,16 @@ fittedValues[outcome=='ipd_cases', cases:=ipd_cases]
 fittedValues[outcome=='ipd_pcv10_serotype_cases', cases:=ipd_pcv10_serotype_cases]
 fittedValues[outcome=='ipd_non_pcv10_serotype_cases', cases:=ipd_non_pcv10_serotype_cases]
 fittedValues[outcome=='xrcp_cases', cases:=xrcp_cases]
+
+# collapse to quarter level
+quarterData = fittedValues[,c('outcome','moyr','cases'),with=F]
+quarterData[month(moyr)<4, moyr:=as.Date(paste0('0102', year(moyr)), '%d%m%Y')]
+quarterData[month(moyr)>=4 & month(moyr)<7, moyr:=as.Date(paste0('0105', year(moyr)), '%d%m%Y')]
+quarterData[month(moyr)>=7 & month(moyr)<10, moyr:=as.Date(paste0('0108', year(moyr)), '%d%m%Y')]
+quarterData[month(moyr)>=10, moyr:=as.Date(paste0('0111', year(moyr)), '%d%m%Y')]
+quarterData = quarterData[, list(mean=mean(cases)), by=c('outcome', 'moyr')]
+fittedValues = merge(fittedValues, quarterData, c('outcome', 'moyr'), all=TRUE)
+fittedValues[, cases:=mean]
 # -----------------------------------------------------------------------------------------
 
 
@@ -106,29 +126,48 @@ fittedValues[outcome=='xrcp_cases', cases:=xrcp_cases]
 # graph settings
 colors1 = brewer.pal(3, 'Paired')
 colors2 = brewer.pal(6, 'GnBu')[-1]
-smoothFormula = y ~ poly(x, 4) + poly(x, 3) + poly(x, 2)
+
+# open pdf
+pdf(graphFile, width=10, height=6)
 
 # graph effect sizes
-ggplot(effectSizes[est=='Estimate'], aes(y=effect, x=run_id)) + 
+p = ggplot(effectSizes[est=='Estimate'], aes(y=effect, x=run_id)) + 
 	geom_hline(yintercept=0, color='red') + 
-	geom_smooth(se=FALSE, method='lm', formula=smoothFormula, color=colors1[2]) + 
-	geom_smooth(data=effectSizes[est=='Lower'], se=FALSE, method='lm', formula=smoothFormula, color=colors1[1]) + 
-	geom_smooth(data=effectSizes[est=='Upper'], se=FALSE, method='lm', formula=smoothFormula, color=colors1[1]) + 
-	geom_point() + 
-	geom_point(data=effectSizes[est=='Lower']) + 
-	geom_point(data=effectSizes[est=='Upper']) + 
+	geom_point(, alpha=.6) + 
+	geom_point(data=effectSizes[est=='Lower'], alpha=.6) + 
+	geom_point(data=effectSizes[est=='Upper'], alpha=.6) + 
+	geom_smooth(se=FALSE, aes(color='Estimate'), size=1.25) + 
+	geom_smooth(data=effectSizes[est=='Lower'], se=FALSE, aes(color='Upper/Lower'), size=1.25) + 
+	geom_smooth(data=effectSizes[est=='Upper'], se=FALSE, aes(color='Upper/Lower'), size=1.25) + 
 	facet_wrap(~outcome_label, scales='free_y') + 
-	labs(title='Effect Size at Varying Window Width', y='Effect Size (% Change)', x='Window Width') + 
+	scale_color_manual('', breaks=c('Estimate', 'Upper/Lower'), values=c(colors1[2], colors1[1])) + 
+	labs(title='Effect Size at Varying Window Width', y='Effect Size (% Change)', x='Window Width (Months)') + 
 	theme_bw()
-
+print(p)	
+	
 # graph fitted values
-ggplot(fittedValues, aes(y=est, x=moyr, color=run_id, group=run_id)) + 
-	geom_line(size=1.25) + 
+p = ggplot(fittedValues, aes(y=est, x=moyr, color=run_id, group=run_id)) + 
+	geom_line() + 
 	geom_point(aes(y=cases), color='#2D358E') + 
 	geom_vline(xintercept=as.numeric(as.Date('2013-04-01')), linetype=5, color='#C0C0C0') +
-	annotate('text', label='PCV Introduction', x=as.Date('2013-04-01'), y=Inf, hjust=1.5, size=3, hjust=1, vjust=-.25, angle=90) +
+	annotate('text', label='PCV Introduction', x=as.Date('2013-04-01'), y=Inf, hjust=1.1, size=3, hjust=1, vjust=-.25, angle=90) +
 	facet_wrap(~outcome_label, scales='free_y') + 
 	labs(title='Fitted Values at Varying Window Length', y='Expected Cases', x='') + 
-	scale_color_gradientn('Window Width', colors=colors2) + 
+	scale_color_gradientn('Window\nWidth\n(Months)', colors=colors2) + 
 	theme_bw()
+print(p)	
+
+# graph fitted values for PCV10 serotypes only
+p = ggplot(fittedValues[outcome=='ipd_pcv10_serotype_cases'], aes(y=est, x=moyr, color=run_id, group=run_id)) + 
+	geom_line() + 
+	geom_point(aes(y=cases), color='#2D358E') + 
+	geom_vline(xintercept=as.numeric(as.Date('2013-04-01')), linetype=5, color='#C0C0C0') +
+	annotate('text', label='PCV Introduction', x=as.Date('2013-04-01'), y=Inf, hjust=1.1, size=3, hjust=1, vjust=-.25, angle=90) +
+	labs(title='Fitted Values at Varying Window Length\nPCV10 Serotypes', y='Expected Cases', x='') + 
+	scale_color_gradientn('Window\nWidth\n(Months)', colors=colors2) + 
+	theme_bw()
+print(p)	
+
+# close pdf
+dev.off()
 # ---------------------------------------------------------------------------------------------------------
