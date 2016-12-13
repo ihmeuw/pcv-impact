caa7ce22b173f2fc80cc9f6be536eda84aba9638
David Phillips <davidp6@uw.edu>
2016-12-13 12:37:08 -0800

    Remove exposure from ITS

--- its.r
+++ its.r
@@ -70,7 +70,6 @@ its = function(data=NULL, outcome=NULL, cutpoint=NULL, slope=NULL, newEffectDate
 	if (slope & C==1) f = paste(paste(formulaVars, collapse=' ~ '), '+ daysPostIntervention')
 	if (!slope & C==2) f = paste(paste(formulaVars, collapse=' ~ '), '+ duringIntervention + postIntervention')
 	if (slope & C==2) f = paste(paste(formulaVars, collapse=' ~ '), '+ daysDuringIntervention + daysPostIntervention')
-	f = paste0(f, ' + offset(log(', gsub('cases', 'exposure', outcome), '))') # add exposure
 	f = as.formula(f)
 	
 	# run regression
